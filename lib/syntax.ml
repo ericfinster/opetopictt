09b3decb65370169744f3c9b18c49fafd26bf803
Eric Finster <ericfinster@gmail.com>
2021-02-12 12:04:16 +0100

    inverstion of patterns

--- lib/syntax.ml
+++ lib/syntax.ml
@@ -26,7 +26,11 @@ type expr =
 
 type idx = int
 type mvar = int
-  
+
+type bd =
+  | Bound
+  | Defined
+
 type term =
   | VarT of idx
   | LamT of name * term
@@ -34,6 +38,7 @@ type term =
   | PiT of name * term * term
   | TypT
   | MetaT of mvar
+  | InsMetaT of mvar * bd suite
 
 (*****************************************************************************)
 (*                                   Values                                  *)
@@ -55,16 +60,18 @@ and closure =
   | Closure of env * term
 
 (*****************************************************************************)
-(*                            Metavarible Context                            *)
+(*                           Metavariable Context                            *)
 (*****************************************************************************)
 
 type meta_entry =
   | Solved of value
   | Unsolved
 
-type intmap = (mvar,meta_entry,Int.comparator_witness) Map.t
+type metamap = (mvar,meta_entry,Int.comparator_witness) Map.t
 
-let metacontext : intmap ref =
+let next_meta : int ref = ref 0
+    
+let metacontext : metamap ref =
   ref (Map.empty (module Int))
 
 exception Meta_error of string
@@ -88,7 +95,8 @@ let rec eval rho tm =
   | AppT (u,v) -> appV (eval rho u) (eval rho v)
   | PiT (nm,u,v) -> PiV (nm, eval rho u, Closure (rho,v))
   | TypT -> TypV
-  | MetaT m -> metaV m 
+  | MetaT m -> metaV m
+  | InsMetaT (m,bds) -> appBdsV rho (metaV m) bds
 
 and metaV m =
   match lookup_meta m with
@@ -106,11 +114,18 @@ and appV t u =
   | LamV (_,cl) -> cl $$ u 
   | _ -> raise (Eval_error "malformed app")
 
+and appBdsV rho v bds =
+  match (rho , bds) with
+  | (Emp,Emp) -> v
+  | (Ext (rho',u),Ext (bds',Bound)) -> appV (appBdsV rho' v bds') u 
+  | (Ext (rho',_),Ext (bds',Defined)) -> appBdsV rho' v bds'
+  | _ -> raise (Eval_error "malfomed bounding mask")
+
 let rec appSpV v sp =
   match sp with
   | Emp -> v
   | Ext (sp',u) -> appV (appSpV v sp') u
-
+    
 let rec mforce v =
   match v with
   | FlexV (m,sp) ->
@@ -119,3 +134,78 @@ let rec mforce v =
      | Solved v -> mforce (appSpV v sp))
   | _ -> v
        
+(*****************************************************************************)
+(*                                  Quoting                                  *)
+(*****************************************************************************)
+
+let lvl_to_idx k l = k - l - 1
+
+let rec quote k v =
+  match v with
+  | FlexV (m,sp) -> quote_sp k (MetaT m) sp
+  | RigidV (l,sp) -> quote_sp k (VarT (lvl_to_idx k l)) sp
+  | LamV (nm,cl) -> LamT (nm, quote (k+1) (cl $$ RigidV (k,Emp)))
+  | PiV (nm,u,cl) -> PiT (nm, quote k u, quote (k+1) (cl $$ RigidV (k,Emp)))
+  | TypV -> TypT
+
+and quote_sp k t sp =
+  match sp with
+  | Emp -> t
+  | Ext (sp',u) ->
+    AppT (quote_sp k t sp',quote k u)
+
+let nf rho tm =
+  quote (length rho) (eval rho tm) 
+
+(*****************************************************************************)
+(*                                Unification                                *)
+(*****************************************************************************)
+
+let fresh_meta bds =
+  let mctx = ! metacontext in
+  let m = ! next_meta in
+  next_meta := m + 1;
+  metacontext := Map.set mctx ~key:m ~data:Unsolved;
+  InsMetaT (m,bds)
+
+type perm = (lvl,lvl,Int.comparator_witness) Map.t
+                       
+type partial_ren = {
+  dom : lvl;
+  cod : lvl;
+  ren : perm;
+}
+
+let lift pren = {
+  dom = pren.dom + 1;
+  cod = pren.cod + 1;
+  ren = Map.set pren.ren ~key:pren.cod ~data:pren.dom; 
+}
+
+exception Unify_error of string
+    
+let invert cod sp =
+  let rec go = function
+    | Emp -> (0, Map.empty (module Int))
+    | Ext (sp',u) ->
+      let (dom, ren) = go sp' in
+      (match mforce u with
+       | RigidV (l,Emp) ->
+         (match Map.add ren ~key:l ~data:dom  with
+          | `Ok ren' -> (dom + 1,ren')
+          | `Duplicate -> raise (Unify_error "non-linear pattern"))
+       | _ -> raise (Unify_error "meta-var applied to non-variable")) in 
+  let (dom,ren) = go sp in
+  { dom = dom ; cod = cod ; ren = ren }
+
+
+(*****************************************************************************)
+(*                                  Contexts                                 *)
+(*****************************************************************************)
+
+type ctx = {
+  rho : env;
+  lvl : lvl;
+  types : (name * value) suite;
+  bds : bd suite;
+}
