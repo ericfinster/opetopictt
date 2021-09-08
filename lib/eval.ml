6463c49dd1071f00f499a83328c7a49bb3e695f0
Eric Finster <ericfinster@gmail.com>
2021-09-08 16:06:38 +0200

    massive cleanup

--- lib/eval.ml
+++ lib/eval.ml
@@ -34,118 +34,12 @@ type fib_desc =
   | TypFib
   | NeutralFib
 
+
 (*****************************************************************************)
-(*                                 Evaluation                                *)
+(*                                Eliminators                                *)
 (*****************************************************************************)
 
-let rec eval top loc tm =
-  (* pr "Evaluating: %a@," pp_term tm; *)
-  match tm with
-  | VarT i -> loc i 
-  | TopT nm -> TopV (nm,EmpSp,top nm)
-
-  | LamT (nm,u) ->
-    LamV (nm,fun v -> eval top (ext_loc loc v) u)
-  | AppT (u,v) -> appV (eval top loc u) (eval top loc v) 
-  | PiT (nm,a,b) ->
-    PiV (nm, eval top loc a,
-         fun v -> eval top (ext_loc loc v) b)
-
-  | PairT (u,v) ->
-    PairV (eval top loc u, eval top loc v)
-  | FstT u ->
-    fstV (eval top loc u)
-  | SndT u ->
-    sndV (eval top loc u)
-  | SigT (nm,u,v) ->
-    SigV (nm, eval top loc u,
-          fun x -> eval top (ext_loc loc x) v)
-
-  | CellT (tl,ty,c) ->
-    let (tl_v, ty_v, c_v) =
-      eval_cell_desc top loc tl ty c in
-    cellV tl_v ty_v c_v
-  | CompT (tl,ty,c) ->
-    let (tl_v, ty_v, c_v) =
-      eval_cell_desc top loc tl ty c in
-    CompV  (tl_v , ty_v , c_v)
-  | FillT (tl,ty,c) ->
-    let (tl_v, ty_v, c_v) =
-      eval_cell_desc top loc tl ty c in
-    FillV  (tl_v , ty_v , c_v)
-  | KanElimT (tl,ty,c,p,d,comp,fill) ->
-    let (tl_v, ty_v, c_v) =
-      eval_cell_desc top loc tl ty c in
-    let pv = eval top loc p in
-    let dv = eval top loc d in
-    let compv = eval top loc comp in
-    let fillv = eval top loc fill in
-    kanElimV tl_v ty_v c_v pv dv compv fillv
-
-  | TypT -> TypV
-
-and eval_cell_desc top loc tl ty c =
-  let (tl_v , ty_v) = eval_fib top loc tl ty in 
-  let c_v = map_cell_desc_cmplx c ~f:(eval top loc) in 
-  (tl_v, ty_v, c_v)
-
-and eval_fib top loc tl ty =
-  match tl with
-  | Emp -> (Emp , eval top loc ty)
-  | Ext (tl',(nm,ty')) ->
-
-    let (tl_v , ty_v) = eval_fib top loc tl' ty' in
-    let lams = TermUtil.abstract_tele tl ty in 
-    let this_ty_val = eval top loc lams in
-
-    (Ext (tl_v,(nm,ty_v)) , this_ty_val) 
-
-and get_fib_desc tl ty =
-  match app_vars tl ty 0 with 
-  | (SigV (nm,_,_),_) ->
-
-    let ext_sig_src v =
-      begin match v with
-        | SigV (_,a,_) -> a
-        | _ -> failwith "ext_sig_src"
-      end in
-
-    let ext_sig_tgt v = 
-      begin match v with
-        | SigV (nm,_,b) -> LamV (nm,b)
-        | _ -> failwith "ext_sig_tgt"
-      end in 
-
-    let tlst = to_list tl in
-    let a = map_fib tlst ty ext_sig_src in
-    let b = map_fib tlst ty ext_sig_tgt in
-
-    SigFib (nm,a,b)
-
-  | (PiV (nm,_,_),_) ->
-
-    let ext_pi_src v =
-      begin match v with
-        | PiV (_,a,_) -> a
-        | _ -> failwith "ext_pi_src"
-      end in
-
-    let ext_pi_tgt v = 
-      begin match v with
-        | PiV (nm,_,b) -> LamV (nm,b)
-        | _ -> failwith "ext_pi_tgt"
-      end in 
-
-    let tlst = to_list tl in
-    let a = map_fib tlst ty ext_pi_src in
-    let b = map_fib tlst ty ext_pi_tgt in
-
-    PiFib (nm,a,b)
-
-  | (TypV,_) -> TypFib
-  | _ -> NeutralFib
-
-and appV t u =
+let rec appV t u =
   match t with
   | RigidV (i,sp) -> RigidV (i,AppSp(sp,u))
   | TopV (nm,sp,tv) -> TopV (nm,AppSp(sp,u), appV tv u)
@@ -154,27 +48,204 @@ and appV t u =
   | LamV (_,cl) -> cl u
   | _ -> raise (Eval_error (Fmt.str "malformed application: %a" pp_value t))
 
-and fstV t =
+let rec app_to_fib v_lst ty =
+  match v_lst with
+  | [] -> ty
+  | v::vs -> app_to_fib vs (appV ty v)
+
+(* TODO: Combine the next two functions? *)
+let rec app_vars t v i =
+  match t with
+  | Emp -> (v,i)
+  | Ext (t',(_,_)) ->
+    let (v',k) = app_vars t' v i in 
+    (appV v' (varV k), k + 1)
+
+let rec fstV t =
   match t with
   | RigidV (i,sp) -> RigidV (i, FstSp sp)
   | TopV (nm,sp,tv) -> TopV (nm, FstSp sp, fstV tv)
   | PairV (u,_) -> u
   | _ -> raise (Eval_error (Fmt.str "malformed first proj: %a" pp_value t))
 
-and sndV t =
+let rec sndV t =
   match t with
   | RigidV (i,sp) -> RigidV (i, SndSp sp)
   | TopV (nm,sp,tv) -> TopV (nm, SndSp sp, sndV tv)
   | PairV (_,v) -> v
   | _ -> raise (Eval_error (Fmt.str "malformed second proj: %a" pp_value t))
 
-and cellV tl ty c =
+
+(*****************************************************************************)
+(*                            Complex Combinators                            *)
+(*****************************************************************************)
+
+let fstC (c : value dep_term cmplx) : value dep_term cmplx
+  = map_cmplx c
+    ~f:(fun (vs,vo) -> (vs,Option.map vo ~f:fstV))
+
+let sndC (c : value dep_term cmplx) (v : value) : value dep_term cmplx
+  = map_cmplx c
+    ~f:(fun (vs,vo) ->
+        match vo with
+        | None -> (Ext (vs,v),None)
+        | Some p -> (Ext (vs,fstV p),Some (sndV p)))
+
+(* This one is debateable.  Probably we should rather 
+   adjoin the argument to the dep_term as a separate step ...*)
+let appC (c : value dep_term cmplx) (arg_c : value cmplx) = 
+  match_cmplx (face_cmplx arg_c) c
+    ~f:(fun f (ss,so) ->
+        let args = labels f in
+        let top_arg = base_value (head_of f) in
+        (Ext (ss,top_arg), Option.map so ~f:(app_to_fib args)))
+
+(* Abstract over all the positions in a given complex and pass
+   the abstracted values in complex form to the function b. *)
+let rec lamC (a : 'a cmplx) (b : value cmplx -> value) : value =
+
+  let rec do_lams nl cm =
+    match nl with
+    | [] -> b cm
+    | addr::addrs ->
+      LamV ("", fun v -> 
+          do_lams addrs (replace_at cm (0,addr) v))
+
+  in match a with
+  | Base n ->
+    let n' = map_nst_with_addr n
+        ~f:(fun _ addr -> addr) in
+    let nv = map_nst n ~f:(fun _ -> TypV) in 
+    do_lams (nodes_nst n') (Base nv)
+  | Adjoin (t,n) ->
+    let n' = map_nst_with_addr n
+        ~f:(fun _ addr -> addr) in 
+    let nv = map_nst n ~f:(fun _ -> TypV) in 
+    lamC t (fun vc -> do_lams (nodes_nst n') (Adjoin (vc,nv)))
+
+(* Given the fibration a and a collection of cell values for the 
+   telescopes, return the value cmplx whose cells are decorated 
+   by the cell fibrations generated by a *)
+let rec cellC (tl : value tele) (a : value) (args : value suite cmplx) : value cmplx =
+
+  match args with
+  | Base n ->
+
+    let n' = map_nst n ~f:(fun vs -> app_to_fib (to_list vs) a) 
+    in Base n'
+
+  | Adjoin (t,n) ->
+
+    let t' = cellC tl a t in
+
+    let n' = map_nst_with_addr n
+        ~f:(fun _ addr ->
+            let f = face_at args (0,addr) in
+            let f_tl = tail_of f in 
+            let cell_fib = lamC f_tl (fun vc ->
+                let dtc = match_cmplx vc f_tl
+                    ~f:(fun v vs -> (vs,Some v)) in
+                let hd = map_nst (head_of f)
+                    ~f:(fun vs -> (vs,None)) in 
+                CellV (tl,a, Adjoin (dtc,hd))) in
+            cell_fib) in 
+
+    Adjoin (t',n')
+
+(* Given a "complex dependent type", extract the space 
+   of sections of it with respect to the complex indexed 
+   fibration b *)
+let rec piC (a : value cmplx) (b : value cmplx -> value) : value =
+
+  let rec do_pis nl cm =
+    match nl with
+    | [] -> b cm
+    | (addr,typ)::ns ->
+      PiV ("", typ, fun v ->
+          do_pis ns (replace_at cm (0,addr) v))
+
+  in match a with
+  | Base n ->
+
+    let n' = map_nst_with_addr n
+        ~f:(fun typ addr -> (addr,typ)) in
+    do_pis (nodes_nst n') a
+
+  | Adjoin (t,n) ->
+
+    piC t (fun vc ->
+
+        let n' = map_nst_with_addr n
+            ~f:(fun fib addr ->
+                let f = face_at (Adjoin (vc,n)) (0,addr) in
+                let f_tl = tail_of f in
+                let typ = app_to_fib (labels f_tl) fib in 
+                (addr,typ)) in
+
+        do_pis (nodes_nst n') (Adjoin (vc,n))
+
+      )
+
+(*****************************************************************************)
+(*                           Fibration Inspection                            *)
+(*****************************************************************************)
+
+let rec map_fib t v f =
+  match t with
+  | [] -> f v 
+  | (nm,_)::tps ->
+    LamV (nm,fun x ->
+        let tps' = List.map tps ~f:(fun (nm,y) -> (nm,appV y x)) in
+        let v' = appV v x in 
+        map_fib tps' v' f)
+
+let describe_fib tl ty =
+  match app_vars tl ty 0 with 
+  | (SigV (nm,_,_),_) ->
+
+    let tlst = to_list tl in
+    let a = map_fib tlst ty
+        (function
+          | SigV (_,a,_) -> a
+          | _ -> failwith "ext_sig_src") in 
+    
+    let b = map_fib tlst ty
+        (function
+          | SigV (nm,_,b) -> LamV (nm,b)
+          | _ -> failwith "ext_sig_tgt") in
+
+    SigFib (nm,a,b)
+
+  | (PiV (nm,_,_),_) ->
+
+    let tlst = to_list tl in
+    let a = map_fib tlst ty
+        (function
+          | PiV (_,a,_) -> a
+          | _ -> failwith "ext_pi_src") in 
+    
+    let b = map_fib tlst ty
+        (function
+          | PiV (nm,_,b) -> LamV (nm,b)
+          | _ -> failwith "ext_pi_tgt") in
+
+    PiFib (nm,a,b)
+
+  | (TypV,_) -> TypFib
+  | _ -> NeutralFib
+
+
+(*****************************************************************************)
+(*                           Cell Type Calculation                           *)
+(*****************************************************************************)
+
+let rec cellV tl ty c =
   match c with
   | Base (Lf (vs,_)) ->
     app_to_fib (to_list vs) ty 
   | _ ->
 
-    begin match get_fib_desc tl ty with
+    begin match describe_fib tl ty with
 
       | SigFib (nm,a,b) ->
 
@@ -245,37 +316,6 @@ and cellV tl ty c =
 
     end
 
-and kanElimV tl ty c p d comp fill =
-  match (comp,fill) with
-  | (TopV (_,_,tv),_) ->
-    kanElimV tl ty c p d tv fill
-  | (_,TopV (_,_,tv)) ->
-    kanElimV tl ty c p d comp tv
-  (* TODO : What kind of check do I need here? *) 
-  | (CompV (_,_,_) , FillV (_,_,_)) -> d
-  | _ -> KanElimV (tl,ty,c,p,d,comp,fill,EmpSp)
-
-(* TODO: Combine the next two functions? *)
-and app_vars t v i =
-  match t with
-  | Emp -> (v,i)
-  | Ext (t',(_,_)) ->
-    let (v',k) = app_vars t' v i in 
-    (appV v' (varV k), k + 1)
-
-and app_to_fib v_lst ty =
-  match v_lst with
-  | [] -> ty
-  | v::vs -> app_to_fib vs (appV ty v)
-
-and map_fib t v f =
-  match t with
-  | [] -> f v 
-  | (nm,_)::tps ->
-    LamV (nm,fun x ->
-        let tps' = List.map tps ~f:(fun (nm,y) -> (nm,appV y x)) in
-        let v' = appV v x in 
-        map_fib tps' v' f)
 
 and abst_type_lst nl cm k =
   match nl with
@@ -285,41 +325,6 @@ and abst_type_lst nl cm k =
         abst_type_lst ns
           (apply_at cm (0,addr)
              (fun (vs,_) -> (vs, Some v))) k)
-
-(* A general routine for abstracting over the 
-   variables of a complex.  This maybe belongs elsewhere ... *)
-and abst_cmplx (tl : value tele) (ty : value)
-    (c : value dep_term cmplx)
-    (k : value dep_term cmplx -> value) : value =
-
-  match c with
-  | Base n ->
-
-    let typ_nst = map_nst_with_addr n
-        ~f:(fun (ts,_) addr ->
-            let this_typ = app_to_fib (to_list ts) ty in
-            (addr,this_typ)) in 
-
-    abst_type_lst (nodes_nst typ_nst) (Base n) k 
-
-  | Adjoin (t,n) -> 
-
-    let k' t' = 
-
-      let frm_cmplx = Adjoin (t',n) in
-
-      let typ_nst = map_nst_with_addr n
-          ~f:(fun (_,_) addr ->
-              let f = face_at frm_cmplx (0,addr) in
-              (* zero our the term position just in case ... *)
-              let f' = with_head f
-                  (map_nst (head_of f) ~f:(fun (vs,_) -> (vs,None))) in 
-              let this_typ = CellV (tl,ty,f') 
-              in (addr,this_typ))
-
-      in abst_type_lst (nodes_nst typ_nst) frm_cmplx k 
-
-    in abst_cmplx tl ty t k' 
 
 (* Similar to above, but abstracts over a sequence of fibrations in 
    the universe ...*)
@@ -359,116 +364,87 @@ and cell_univ
 
     in cell_univ t k' 
 
+(*****************************************************************************)
+(*                              Kan Calculation                              *)
+(*****************************************************************************)
+
+let rec kanElimV tl ty c p d comp fill =
+  match (comp,fill) with
+  | (TopV (_,_,tv),_) ->
+    kanElimV tl ty c p d tv fill
+  | (_,TopV (_,_,tv)) ->
+    kanElimV tl ty c p d comp tv
+  (* TODO : What kind of check do I need here? *) 
+  | (CompV (_,_,_) , FillV (_,_,_)) -> d
+  | _ -> KanElimV (tl,ty,c,p,d,comp,fill,EmpSp)
+
 
 (*****************************************************************************)
-(*                            Complex Combinators                            *)
+(*                                 Evaluation                                *)
 (*****************************************************************************)
 
-and fstC (c : value dep_term cmplx) : value dep_term cmplx
-  = map_cmplx c
-    ~f:(fun (vs,vo) -> (vs,Option.map vo ~f:fstV))
+let rec eval top loc tm =
+  (* pr "Evaluating: %a@," pp_term tm; *)
+  match tm with
+  | VarT i -> loc i 
+  | TopT nm -> TopV (nm,EmpSp,top nm)
 
-and sndC (c : value dep_term cmplx) (v : value) : value dep_term cmplx
-  = map_cmplx c
-    ~f:(fun (vs,vo) ->
-        match vo with
-        | None -> (Ext (vs,v),None)
-        | Some p -> (Ext (vs,fstV p),Some (sndV p)))
+  | LamT (nm,u) ->
+    LamV (nm,fun v -> eval top (ext_loc loc v) u)
+  | AppT (u,v) -> appV (eval top loc u) (eval top loc v) 
+  | PiT (nm,a,b) ->
+    PiV (nm, eval top loc a,
+         fun v -> eval top (ext_loc loc v) b)
 
-(* This one is debateable.  Probably we should rather 
-   adjoin the argument to the dep_term as a separate step ...*)
-and appC (c : value dep_term cmplx) (arg_c : value cmplx) = 
-  match_cmplx (face_cmplx arg_c) c
-    ~f:(fun f (ss,so) ->
-        let args = labels f in
-        let top_arg = base_value (head_of f) in
-        (Ext (ss,top_arg), Option.map so ~f:(app_to_fib args)))
+  | PairT (u,v) ->
+    PairV (eval top loc u, eval top loc v)
+  | FstT u ->
+    fstV (eval top loc u)
+  | SndT u ->
+    sndV (eval top loc u)
+  | SigT (nm,u,v) ->
+    SigV (nm, eval top loc u,
+          fun x -> eval top (ext_loc loc x) v)
 
-(* Abstract over all the positions in a given complex and pass
-   the abstracted values in complex form to the function b. *)
-and lamC (a : 'a cmplx) (b : value cmplx -> value) : value =
+  | CellT (tl,ty,c) ->
+    let (tl_v, ty_v, c_v) =
+      eval_cell_desc top loc tl ty c in
+    cellV tl_v ty_v c_v
+  | CompT (tl,ty,c) ->
+    let (tl_v, ty_v, c_v) =
+      eval_cell_desc top loc tl ty c in
+    CompV  (tl_v , ty_v , c_v)
+  | FillT (tl,ty,c) ->
+    let (tl_v, ty_v, c_v) =
+      eval_cell_desc top loc tl ty c in
+    FillV  (tl_v , ty_v , c_v)
+  | KanElimT (tl,ty,c,p,d,comp,fill) ->
+    let (tl_v, ty_v, c_v) =
+      eval_cell_desc top loc tl ty c in
+    let pv = eval top loc p in
+    let dv = eval top loc d in
+    let compv = eval top loc comp in
+    let fillv = eval top loc fill in
+    kanElimV tl_v ty_v c_v pv dv compv fillv
 
-  let rec do_lams nl cm =
-    match nl with
-    | [] -> b cm
-    | addr::addrs ->
-      LamV ("", fun v -> 
-          do_lams addrs (replace_at cm (0,addr) v))
+  | TypT -> TypV
 
-  in match a with
-  | Base n ->
-    let n' = map_nst_with_addr n
-        ~f:(fun _ addr -> addr) in
-    let nv = map_nst n ~f:(fun _ -> TypV) in 
-    do_lams (nodes_nst n') (Base nv)
-  | Adjoin (t,n) ->
-    let n' = map_nst_with_addr n
-        ~f:(fun _ addr -> addr) in 
-    let nv = map_nst n ~f:(fun _ -> TypV) in 
-    lamC t (fun vc -> do_lams (nodes_nst n') (Adjoin (vc,nv)))
+and eval_cell_desc top loc tl ty c =
+  let (tl_v , ty_v) = eval_fib top loc tl ty in 
+  let c_v = map_cell_desc_cmplx c ~f:(eval top loc) in 
+  (tl_v, ty_v, c_v)
 
-(* Given the fibration a and a collection of cell values for the 
-   telescopes, return the value cmplx whose cells are decorated 
-   by the cell fibrations generated by a *)
-and cellC (tl : value tele) (a : value) (args : value suite cmplx) : value cmplx =
+and eval_fib top loc tl ty =
+  match tl with
+  | Emp -> (Emp , eval top loc ty)
+  | Ext (tl',(nm,ty')) ->
 
-  match args with
-  | Base n ->
+    let (tl_v , ty_v) = eval_fib top loc tl' ty' in
+    let lams = TermUtil.abstract_tele tl ty in 
+    let this_ty_val = eval top loc lams in
 
-    let n' = map_nst n ~f:(fun vs -> app_to_fib (to_list vs) a) 
-    in Base n'
+    (Ext (tl_v,(nm,ty_v)) , this_ty_val) 
 
-  | Adjoin (t,n) ->
-
-    let t' = cellC tl a t in
-
-    let n' = map_nst_with_addr n
-        ~f:(fun _ addr ->
-            let f = face_at args (0,addr) in
-            let f_tl = tail_of f in 
-            let cell_fib = lamC f_tl (fun vc ->
-                let dtc = match_cmplx vc f_tl
-                    ~f:(fun v vs -> (vs,Some v)) in
-                let hd = map_nst (head_of f)
-                    ~f:(fun vs -> (vs,None)) in 
-                CellV (tl,a, Adjoin (dtc,hd))) in
-            cell_fib) in 
-
-    Adjoin (t',n')
-
-(* Given a "complex dependent type", extract the space 
-   of sections of it with respect to the complex indexed 
-   fibration b *)
-and piC (a : value cmplx) (b : value cmplx -> value) : value =
-
-  let rec do_pis nl cm =
-    match nl with
-    | [] -> b cm
-    | (addr,typ)::ns ->
-      PiV ("", typ, fun v ->
-          do_pis ns (replace_at cm (0,addr) v))
-
-  in match a with
-  | Base n ->
-
-    let n' = map_nst_with_addr n
-        ~f:(fun typ addr -> (addr,typ)) in
-    do_pis (nodes_nst n') a
-
-  | Adjoin (t,n) ->
-
-    piC t (fun vc ->
-
-        let n' = map_nst_with_addr n
-            ~f:(fun fib addr ->
-                let f = face_at (Adjoin (vc,n)) (0,addr) in
-                let f_tl = tail_of f in
-                let typ = app_to_fib (labels f_tl) fib in 
-                (addr,typ)) in
-
-        do_pis (nodes_nst n') (Adjoin (vc,n))
-
-      )
 
 (*****************************************************************************)
 (*                                  Quoting                                  *)
